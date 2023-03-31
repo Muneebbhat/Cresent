a5bda67737454d698d47295042b855b34b4111e2
Sawar-Nabi <sawarnabi100@gmail.com>
2023-03-31 16:47:49 +0530

    page index added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./app.css";
-import { Header, ImageSlider } from "./Components";
+import Index from "./pages";
 
 const App = () => {
   return (
-    <div style={{maxWidth:"1366px", width: '100%', margin:'auto', position: 'relative'}}>
-      <Header />
-      <ImageSlider />
+    <div style={{ maxWidth: "1366px", width: "100%", margin: "auto" }}>
+      <Index />
     </div>
   );
 };
