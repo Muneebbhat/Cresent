8496aada9855f671012f9182795983307d70613b
Sawar-Nabi <sawarnabi100@gmail.com>
2023-03-31 15:50:54 +0530

    slider Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import './app.css'
-import { Header } from './Components'
+import React from "react";
+import "./app.css";
+import { Header, ImageSlider } from "./Components";
 
 const App = () => {
   return (
-    <div>
-      <Header/>
+    <div style={{maxWidth:"1366px", width: '100%', margin:'auto', position: 'relative'}}>
+      <Header />
+      <ImageSlider />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
