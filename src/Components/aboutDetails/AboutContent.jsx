148ed413211409215b3501d62064f636a1b69a8b
Muneeb Bhat <muneebbhat1990@gmail.com>
2023-04-03 18:16:13 +0530

    tala wuch

--- src/Components/aboutDetails/AboutContent.jsx
+++ src/Components/aboutDetails/AboutContent.jsx
@@ -3,7 +3,6 @@ import { AboutContainer } from './AboutContentStyle';
 import data from '../../assets/local-data/Data.json';
 
 const AboutContent = () => {
-  console.log(data.about_image);
   return (
     <AboutContainer>
       <div className='image_container'>
@@ -13,11 +12,9 @@ const AboutContent = () => {
         <ul>
           <li>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, magni.
-            Dolorem nulla perferendis optio! Qui, voluptas, ratione rem
           </li>
           <li>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam harum
-            consectetur beatae itaque sequi facere unde quia natus. Molestias
           </li>
           <li>
             vel recusandae maxime sit in iure sunt aperiam quisquam cupiditate.
@@ -25,6 +22,9 @@ const AboutContent = () => {
           <li>maxime optio eveniet velit sed earum. Ipsum, mollitia.</li>
           <li>
             eum cumque debitis est corrupti tenetur corporis. Quae quo laborum
+            atque.
+          </li>
+          <li>
             doloribus voluptates accusantium voluptate corrupti earum maiores
             atque.
           </li>
--- src/Components/aboutDetails/AboutContentStyle.js
+++ src/Components/aboutDetails/AboutContentStyle.js
@@ -3,95 +3,75 @@ import { Constant } from '../../assets/reusable/index';
 
 export const AboutContainer = styled.div`
   /* background-color: green; */
-  height: 80%;
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
+  padding: 0 0 20px 0;
+  min-height: 400px;
   .image_container {
     position: relative;
     width: 40%;
-    height: 100%;
     /* background-color: maroon; */
     display: flex;
-    align-items: center;
     justify-content: center;
     img {
       position: absolute;
-      height: 110%;
-      top: -100px;
+      height: 400px;
+      width: 300px;
+      top: -250px;
     }
   }
   .text_container {
     width: 60%;
     height: 100%;
-
-    padding: 10px 50px 10px 10px;
+    padding-right: 10px;
     /* background-color: maroon; */
     ul {
       list-style: none;
       li {
         font-size: 16px;
-        padding-top: 10px;
+        padding-top: 5px;
       }
     }
   }
 
   @media screen and (max-width: 1024px) {
-    height: 80%;
-    .image_container {
-      width: 40%;
-      height: 100%;
+    /* background-color: red; */
 
+    .image_container {
       img {
-        height: 100%;
-        top: -50px;
-      }
-    }
-    .text_container {
-      padding: 10px 10px 10px 10px;
-      ul {
-        list-style: none;
-        li {
-          font-size: 14px;
-          padding-top: 10px;
-        }
+        position: absolute;
+        height: 400px;
+        width: 300px;
+        top: -220px;
       }
     }
   }
   /* media 768 */
   @media screen and (max-width: 768px) {
+    /* background-color: red; */
+
     .image_container {
       img {
         position: absolute;
-        height: 100%;
-        top: -40px;
-      }
-    }
-    .text_container {
-      width: 60%;
-      height: 100%;
-      padding: 10px 10px 10px 0px;
-      ul {
-        list-style: none;
-        li {
-          font-size: 14px;
-          padding-top: 5px;
-        }
+        height: 370px;
+        width: 250px;
+        top: -220px;
       }
     }
   }
   /* media 425 */
   @media screen and (max-width: 550px) {
-    height: 80%;
-    width: 100%;
+    min-height: 250px;
+
+    padding: 10px 0;
+    background-color: red;
     .image_container {
       display: none;
     }
     .text_container {
       width: 100%;
-      padding: 20px 10px;
-      /* background-color: maroon; */
       ul {
         list-style: none;
         li {
