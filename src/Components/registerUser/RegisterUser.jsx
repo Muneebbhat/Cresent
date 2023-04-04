f3ba22187c53e6b881acc918f5ed7c63227a18b1
Sawar-Nabi <sawarnabi100@gmail.com>
2023-04-04 12:51:38 +0530

    model file created

---
+++ src/Components/registerUser/RegisterUser.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Model } from "./registerStyle";
+
+const RegisterUser = () => {
+  return (
+    <Model>
+      <h1>This is Model Page</h1>
+    </Model>
+  );
+};
+
+export default RegisterUser;
---
+++ src/Components/registerUser/registerStyle.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Constant } from "../../assets/reusable/index";
+
+export const Model = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: red;
+`;
