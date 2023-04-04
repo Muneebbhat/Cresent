0c5a7c7e52dda6e2b1b597ef1cc80d40f2def060
Muneeb Bhat <muneebbhat1990@gmail.com>
2023-04-04 17:55:23 +0530

    header

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,39 +1,46 @@
-import React, { useRef, useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
-import { Link } from "react-scroll";
-import { Navigation } from "./headerStyle";
-import data from "../../assets/local-data/Data.json";
-import Logo from "../../assets/images/logo_creacent.png";
-import { TbMenu2 } from "react-icons/tb";
+import React, { useRef, useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Link } from 'react-scroll';
+import { Navigation } from './headerStyle';
+import data from '../../assets/local-data/Data.json';
+import Logo from '../../assets/images/logo_creacent.png';
+import { TbMenu2 } from 'react-icons/tb';
 
 const Header = ({ sideMunuProp }) => {
   const [headerOffset, setHeaderOffset] = useState(0);
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
+    window.addEventListener('scroll', () => {
       setHeaderOffset(window.pageYOffset);
     });
   }, []);
 
   const anchorTaqs = useRef();
   return (
-    <Navigation opacity={headerOffset >= 320 ? "1" : "0.6"}>
-      <div className="container">
-        <div className="main_container">
+    <Navigation opacity={headerOffset >= 320 ? '1' : '0.6'}>
+      <div className='container'>
+        <div className='main_container'>
           {/* logo Container */}
-          <div className="logo_container">
-            <a href="/" ref={anchorTaqs}>
-              <img src={Logo} alt="Logo" />
+          <div className='logo_container'>
+            <a href='/' ref={anchorTaqs}>
+              <img src={Logo} alt='Logo' />
             </a>
-            <a href="/">Cresent Bakery</a>
+            <a href='/'>Cresent Bakery</a>
           </div>
 
           {/* navigation Container */}
-          <div className="nav_container">
+          <div className='nav_container'>
             <ul>
               {data.navigation.map((currEl, i) => (
                 <li key={i}>
-                  <Link to={currEl} key={i} href={`#${currEl}`} activeClass="active" spy={true}>
+                  <Link
+                    to={currEl}
+                    key={i}
+                    href={`#${currEl}`}
+                    activeClass='active'
+                    spy={true}
+                    smooth={true}
+                  >
                     {currEl}
                   </Link>
                 </li>
@@ -43,7 +50,7 @@ const Header = ({ sideMunuProp }) => {
 
           {/* Side Menu Style Icon */}
 
-          <div className="side_menu_wrapper">
+          <div className='side_menu_wrapper'>
             <TbMenu2 onClick={() => sideMunuProp()} />
           </div>
         </div>
